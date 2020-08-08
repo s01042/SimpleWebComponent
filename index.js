ad52b692902172d080dd99e9f5b652ddbb5d72b5
s01042 <s01042.ms@googlemail.com>
2020-08-08 17:31:06 +0200

    prapering drag and drop

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ import UserCard from './components/userCard.js'
 import ServiceComponent from './components/serviceComponent.js'
 import AppConfig from './components/appConfig.js'
 
-const myAppConfig = new AppConfig(true) //runs in DEV environment
+const myAppConfig = new AppConfig() //runs in DEV environment
 const myServiceComponent = new ServiceComponent(myAppConfig)
 
 /**
